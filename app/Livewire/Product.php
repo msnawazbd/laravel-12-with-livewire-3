d31386e8f04db41a3a271c0614883b1a708fbfb5
Shahid Nawaz <msnawazbd@gmail.com>
2025-07-22 21:10:21 +0600

    Form Validation

--- app/Livewire/Product.php
+++ app/Livewire/Product.php
@@ -6,6 +6,16 @@ use Livewire\Component;
 
 class Product extends Component
 {
+    public $name, $price, $description;
+
+    public function store()
+    {
+        $this->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric|min:0',
+            'description' => 'required|string|max:1000',
+        ]);
+    }
     public function render()
     {
         return view('livewire.product');
--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -1,3 +1,30 @@
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+    <form wire:submit.prevent="store">
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" id="name" class="form-control" wire:model="product.name">
+            @error('name')
+            <p class="text-danger">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="form-group">
+            <label for="price">Price</label>
+            <input type="number" id="price" class="form-control" wire:model="product.price">
+            @error('price')
+            <p class="text-danger">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="description">Description</label>
+            <textarea id="description" class="form-control" wire:model="product.description"></textarea>
+            @error('description')
+            <p class="text-danger">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <button type="submit" class="btn btn-primary">Save</button>
+
+    </form>
 </div>
